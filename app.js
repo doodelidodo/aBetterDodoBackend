cd1ae1308b547cf94a5c2983259922684bb390f0
Dominic Meyer <dominic.meyer@me.com>
2021-07-12 11:29:03 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRoutes = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) Middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+})
+
+app.use('/api/v1/tours', tourRoutes);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
